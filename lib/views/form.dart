149c1b476373f6abf1948cb496289c9c722666f5
ThaysMrtns <thays.martins@nave.org.br>
2021-02-09 16:50:36 -0300

    Adicionando formulário

---
+++ lib/views/form.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class Formulario extends StatefulWidget {
+  @override
+  _FormState createState() => _FormState();
+}
+
+class _FormState extends State<Formulario> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [
+          Padding(
+            padding: EdgeInsets.only(top: 20),
+            child: Text(
+              "Saiba qual a melhor opção para o abastecimento do seu carro", 
+              style: TextStyle(fontSize: 18, color: Colors.grey[700], fontWeight: FontWeight.w300) 
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(top: 10),
+            child: TextField(
+              decoration: InputDecoration(
+                contentPadding: EdgeInsets.symmetric(vertical: 20),
+                labelText: "Preço do Álcool, ex: 3,95",
+                labelStyle: TextStyle(fontSize: 16, color: Colors.grey[600], fontWeight: FontWeight.w300),
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(top: 20),
+            child: TextField(
+              decoration: InputDecoration(
+                contentPadding: EdgeInsets.symmetric(vertical: 20),
+                labelText: "Preço da Gasolina, ex: 5,65",
+                labelStyle: TextStyle(fontSize: 16, color: Colors.grey[600], fontWeight: FontWeight.w300)
+              ),
+            ),
+          ),
+        ]
+      )
+    );
+  }
+}
